7bde0c9c33fd973db0953f4c30dc647be8afdcba
Jacob Frericks <jacob.frericks@gmail.com>
2024-05-05 14:55:18 -0500

    adding pyproject.toml

--- src/snyk_test_normalizer/main.py
+++ src/snyk_test_normalizer/main.py
@@ -9,14 +9,15 @@ def normalize_id(vuln):
         return vuln.get("CWE")[0]
     return ""
 
-def convert(file, update_bome, output_file):
-    if update_bome:
-        bome = update_bome
+def convert(args):
+    if args.update_bome:
+        with open(args.update_bome) as f:
+            bome = json.load(f)
         if not bome.get("vulnerabilities"):
             bome['vulnerabilities'] = []
     else:
         bome = {"vulnerabilities": []}
-    with open(file) as f:
+    with open(args.file) as f:
         snyk_test = json.load(f)
     for vuln in snyk_test.get("vulnerabilities", []):
         bome.get("vulnerabilities").append({
@@ -27,14 +28,14 @@ def convert(file, update_bome, output_file):
             "source": "SNYK",
             "context": ""
         })
-    with open(output_file, 'w') as f:
+    with open(args.output_file, 'w') as f:
         json.dump(bome, f)
 
 
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
-    convert(args.file, args.update_bome, args.output_file)
+    convert(args)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
